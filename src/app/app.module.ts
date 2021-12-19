83557aae73e2c7a60068cf8587b1925e99216a94
Xavier Schuszter <schuszter@etu.unistra.fr>
2021-12-19 21:29:40 +0100

    Mise en place de contrôle formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UserCreationComponent } from './user-creation/user-creation.component';
+import {
+  UserCreationComponent,
+  PhonePipe,
+} from './user-creation/user-creation.component';
 import { CatalogPageComponent } from './catalog-page/catalog-page.component';
 
 @NgModule({
@@ -13,9 +16,11 @@ import { CatalogPageComponent } from './catalog-page/catalog-page.component';
     MainPageComponent,
     UserCreationComponent,
     CatalogPageComponent,
+
+    PhonePipe,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, UserCreationComponent],
 })
 export class AppModule {}
